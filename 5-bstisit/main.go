29f70c035e739440fbe4a76b0f106ad35b126d48
aregy <aregy@are.gy>
2023-03-25 23:36:46 -0700

    Improved tree balancing

--- 5-bstisit/main.go
+++ 5-bstisit/main.go
@@ -6,6 +6,11 @@ import (
 	"strings"
 )
 
+const (
+	MaxInt int = int(^uint(0) >> 1)
+	MinInt int = -int(^uint(0)>>1) - 1
+)
+
 type Node struct {
 	Val   int
 	Left  *Node
@@ -29,6 +34,29 @@ func (node *Node) StringHelper(n int) string {
 	repr += "\n"
 	repr += leftRightConcat(node.Left.StringHelper(n-1), node.Right.StringHelper(n-1))
 	return repr
+}
+
+func IsBalancedTree(node *Node, interval ...int) bool {
+	if node == nil {
+		return true
+	}
+	if len(interval) >= 1 && interval[0] > node.Val {
+		fmt.Println(interval[0], "≮", node.Val)
+		return false
+	}
+	if len(interval) >= 2 && interval[1] < node.Val {
+		fmt.Println(interval[1], "≯", node.Val)
+		return false
+	}
+	lmin, rmax := MinInt, MaxInt
+	if len(interval) >= 1 {
+		lmin = interval[0]
+	}
+	if len(interval) >= 2 {
+		rmax = interval[1]
+	}
+
+	return IsBalancedTree(node.Left, lmin, min(node.Val, rmax)) && IsBalancedTree(node.Right, max(node.Val, lmin), rmax)
 }
 func min(n ...int) int {
 	if len(n) == 0 {
@@ -88,8 +116,17 @@ func getDepth(node *Node, level int) int {
 	return right
 }
 
+func generateMessage(t *Node) {
+	if IsBalancedTree(t) {
+		fmt.Printf("%v is balanced\n", t)
+	} else {
+		fmt.Printf("%v is unbalanced\n.", t)
+	}
+}
+
 func main() {
 	var t Node = Node{1, &Node{11, &Node{22, &Node{333, nil, nil}, &Node{444, nil, nil}}, nil}, &Node{33, &Node{444, nil, nil}, &Node{444, nil, nil}}}
-	fmt.Println(&t)
-	fmt.Println(getDepth(&t, 0))
+	generateMessage(&t)
+	t = Node{8, &Node{6, &Node{4, &Node{3, nil, nil}, &Node{5, nil, nil}}, nil}, &Node{11, &Node{10, nil, nil}, &Node{12, nil, nil}}}
+	generateMessage(&t)
 }
