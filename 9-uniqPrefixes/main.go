4f28eab34c49d1f30cd060d5156cbc12524c3b74
aregy <aregy@are.gy>
2023-03-29 23:02:07 -0700

    uniq prefixes added

---
+++ 9-uniqPrefixes/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func uniqPrefixes(words *[]string) {
+
+	for i := 0; i < len(*words); i++ {
+		width := 1
+
+		for j := 0; j < len(*words); j++ {
+			if i == j {
+				continue
+			}
+			for k := 0; k < len((*words)[i]); k++ {
+				if (*words)[i][:width] == (*words)[j][:width]{
+					width += 1
+				}
+			}
+		}
+		(*words)[i] = (*words)[i][:width]
+	}
+}
+
+func main() {
+
+	v1 := []string{"joma", "john", "jack", "techlead"}
+	fmt.Println(v1)
+	uniqPrefixes(&v1)
+	fmt.Println(v1)
+	//  ["jom", "joh", "ja", "t"]
+}
