a9f34ab48dce9ac8ea585a4796fe1f18b185dd36
aregy <aregy@are.gy>
2023-03-29 23:06:32 -0700

    unique prefixes without unnecessary layer of indirection

--- 9-uniqPrefixes/main.go
+++ 9-uniqPrefixes/main.go
@@ -4,22 +4,22 @@ import (
 	"fmt"
 )
 
-func uniqPrefixes(words *[]string) {
+func uniqPrefixes(words []string) {
 
-	for i := 0; i < len(*words); i++ {
+	for i := 0; i < len(words); i++ {
 		width := 1
 
-		for j := 0; j < len(*words); j++ {
+		for j := 0; j < len(words); j++ {
 			if i == j {
 				continue
 			}
-			for k := 0; k < len((*words)[i]); k++ {
-				if (*words)[i][:width] == (*words)[j][:width]{
+			for k := 0; k < len(words[i]); k++ {
+				if words[i][:width] == words[j][:width]{
 					width += 1
 				}
 			}
 		}
-		(*words)[i] = (*words)[i][:width]
+		words[i] = words[i][:width]
 	}
 }
 
@@ -27,7 +27,7 @@ func main() {
 
 	v1 := []string{"joma", "john", "jack", "techlead"}
 	fmt.Println(v1)
-	uniqPrefixes(&v1)
+	uniqPrefixes(v1)
 	fmt.Println(v1)
 	//  ["jom", "joh", "ja", "t"]
 }
