682c0536e52fa5ab466bac7a3f1d0bb37dae715e
aregy <aregy@are.gy>
2023-04-09 18:44:38 -0700

    qsort

---
+++ by_topic/qsort.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func partition(nums []int, l int, r int) {
+	if r-l < 1 {
+		return
+	}
+	pivot := nums[r]
+	index := l - 1
+	for i := l; i < r; i++ {
+		if nums[i] < pivot {
+			fmt.Println(nums)
+			nums[index+1], nums[i] = nums[i], nums[index+1]
+			index++
+		}
+	}
+	nums[index+1], nums[r] = nums[r], nums[index+1]
+	index++
+	//if index-1 > l {
+	partition(nums, l, index-1)
+	//}
+	//if index+1 <= r {
+	partition(nums, index+1, r)
+	//}
+}
+
+func qsort(nums []int) {
+	partition(nums, 0, len(nums)-1)
+}
+
+func stat(nums []int) {
+	fmt.Println(nums)
+	qsort(nums)
+	fmt.Println(nums)
+}
+
+func main() {
+	stat([]int{4, 4, 9, 1, 2, -3, 3, 10, 11, 8})
+}
