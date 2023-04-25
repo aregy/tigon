4e67048ad56ae652537b511f4ce5bef776867dd2
aregy <aregy@are.gy>
2023-04-24 22:28:46 -0700

    py reversal stub

---
+++ by_date/23/04/14/main.py
@@ -0,0 +1,12 @@
+WS = { "\t": True, " " : True, "\n" : True }
+
+def reverseWords(s : str) -> str:
+    t = list(s)
+    d = dict()
+    if t[0]
+    return ''.join(t)
+
+if __name__ == "__main__":
+    while True:
+        s : str = input(": ")
+        print(reverseWords(s)l)
