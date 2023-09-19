7638f42f03e20e2f88f9481fd64259232f6bea20
Areg Yeghnazar (DevExpress) <aregy@devexpress.com>
2023-09-19 16:26:18 -0700

    document management in C#: deleting PDF pages

---
+++ by_topic/docs/dotnet1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Running document tasks");
+var args1 = Environment.GetCommandLineArgs();
+// foreach (var a in args) {
+// 	Console.WriteLine(a);
+// }
+Console.WriteLine("---");
+var fileName = args1[1];
+var outPath = args1[2];
+foreach (var a in args1) {
+	Console.WriteLine(a);
+}
+var docProcessor = new DevExpress.Pdf.PdfDocumentProcessor();
+docProcessor.LoadDocument(fileName);
+for (var i = docProcessor.Document.Pages.Count; i > 1; i--){
+	docProcessor.DeletePage(i);
+}
+docProcessor.SaveDocument(outPath);
